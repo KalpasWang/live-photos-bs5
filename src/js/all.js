d6e589e033ba29f617fc6af03bc7dd4bca332e43
Kalpas Wang <wf6212@gmail.com>
2021-06-15 16:12:06 +0800

    加上form validation

--- src/js/all.js
+++ src/js/all.js
@@ -56,3 +56,23 @@ accordionBtns.forEach((btn) => {
     e.target.parentElement.parentElement.classList.toggle('accordion-shadow');
   });
 });
+
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.querySelectorAll('.needs-validation');
+var selectProps = document.getElementById('selectProgram');
+
+// Loop over them and prevent submission
+Array.prototype.slice.call(forms).forEach(function (form) {
+  form.addEventListener(
+    'submit',
+    function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+
+      form.classList.add('was-validated');
+    },
+    false
+  );
+});
